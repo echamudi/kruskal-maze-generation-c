a55580037635912e8e072437aa11d0a58878a1d4
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-29 19:27:56 +0300

    Initial Commit

---
+++ kruskal-maze-generation-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  kruskal-maze-generation-c
+//
+//  Created by Ezzat Chamudi on 11/29/19.
+//  Copyright © 2019 Ezzat Chamudi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
