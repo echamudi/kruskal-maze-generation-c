c8912ce0d58b4448fa0e1cc7c152cf9b7ab4bda7
Ezzat Chamudi <chamudi.git@outlook.com>
2019-12-03 08:16:24 +0300

    Move print_maze function into a new file

--- main.c
+++ main.c
@@ -15,6 +15,8 @@
 
 #include <stdbool.h>
 
+#include "print_maze.h"
+
 const int TOTAL_DIRECTIONS = 12;
 
 const int TOP_LEFT = 0;
@@ -487,50 +489,6 @@ int ** randomized_kruskal(bool verbose) {
     free(maze_draft);
     
     return graph;
-}
-
-void print_maze(int ** maze_graph, int size) {
-    int current_node;
-
-    for (int y = 0; y < size; y++)
-    {
-        for (int x = 0; x < size; x++)
-        {
-            // Check if there's connection to the above node.
-            current_node = x * size + y;
-
-            if (current_node - 1 >= 0 && maze_graph[current_node][current_node - 1] == 1) {
-                printf("██  ");
-            } else {
-                printf("████");
-            }
-        }
-
-        // Right most border
-        printf("██\n");
-
-        for (int x = 0; x < size; x++)
-        {
-            // Check if there's connection to the left node.
-            current_node = x * size + y;
-            
-            if (current_node - size >= 0 && maze_graph[current_node][current_node - size] == 1) {
-                printf("    ");
-            } else {
-                printf("██  ");
-            }
-        }
-
-        // Right most border
-        printf("██\n");
-    }
-
-    // Bottom border
-    for (int i = 0; i < size; i++)
-    {
-        printf("████");
-    }
-    printf("██\n");
 }
 
 int main(int argc, const char * argv[]) {
