ff3ec824d0bd71148ac403698ccf26aa6d533f54
Ezzat Chamudi <chamudi.git@outlook.com>
2019-12-06 13:42:24 +0300

    Add emscripten compilation script

---
+++ emscripten_compile.sh
@@ -0,0 +1,21 @@
+emcc \
+    web.c \
+    definitions.c \
+    print_maze_draft.c \
+    print_maze.c \
+    randomized_kruskal.c \
+    stats.c \
+    util.c \
+    \
+    -o ./dist/script.js \
+    \
+    -s EXPORTED_FUNCTIONS='[
+        "_web_randomized_kruskal",
+        "_randomized_kruskal"
+    ]' \
+    \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS='[
+        "ccall",
+        "cwrap"
+    ]' \
+    \
